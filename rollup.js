c3103f7f2c96268f6f4f0cdfe7db80e1253542dd
Michael Shick <m@shick.us>
2015-10-28 10:26:46 -0700

    maybe not necessary to rollup dist here?

--- rollup.js
+++ rollup.js
@@ -1,19 +1,19 @@
 /* eslint no-console:0 */
 
 import { rollup } from "rollup";
-import npm from "rollup-plugin-npm";
 import babel from "rollup-plugin-babel";
-import inject from "rollup-plugin-inject";
-import commonjs from "rollup-plugin-commonjs";
+// import npm from "rollup-plugin-npm";
+// import inject from "rollup-plugin-inject";
+// import commonjs from "rollup-plugin-commonjs";
 
 
 const input = {
   entry: "lib/knockout-railway.js",
-  external: ["object-assign"],
+  // external: ["object-assign"],
   plugins: [
-    npm({ jsnext: true, main: true }),
-    commonjs(),
-    inject({ "Object.assign": "object-assign" }),
+    // npm({ jsnext: true, main: true }),
+    // commonjs(),
+    // inject({ "Object.assign": "object-assign" }),
     babel()
   ]
 };
